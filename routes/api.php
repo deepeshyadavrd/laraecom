01346f2baadbcefdc2a895c662c3413b4a827477
deepeshyadavrd <raoshsbh6@gmail.com>
2025-10-18 13:24:49 +0530

    created migration for model and controllers, users, category, products, order

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,15 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/products', [ProductController::class, 'index']);
 // Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+use App\Http\Controllers\Admin\CategoryController;
+// use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\OrderController;
+
+Route::prefix('admin')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('orders', OrderController::class);
+});
